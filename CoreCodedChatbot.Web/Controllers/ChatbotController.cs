21af57eaa12c8c141ac4b4217eea9eaded0ebb52
Codedghost <codedghost2@gmail.com>
2018-04-24 17:35:55 +0100

    Renaming controller to something more sensible and including recent version of bootstrap etc

--- CoreCodedChatbot.Web/Controllers/ChatbotController.cs
+++ CoreCodedChatbot.Web/Controllers/ChatbotController.cs
@@ -8,21 +8,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodedChatbot.Web.Controllers
 {
-    public class PlaylistController : Controller
+    public class ChatbotController : Controller
     {
         private readonly PlaylistHelper playlistHelper;
 
-        public PlaylistController(PlaylistHelper playlistHelper)
+        public ChatbotController(PlaylistHelper playlistHelper)
         {
             this.playlistHelper = playlistHelper;
         }
 
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult List()
+        {
             var playlistModel = new PlaylistBrowserSource
             {
                 Songs = playlistHelper.GetAllSongs()
-        };
+            };
 
             return View(playlistModel);
         }
