5087357bf21bf8335999e6ef804d4342d364627e
Codedghost <codedghost2@gmail.com>
2018-08-24 23:24:21 +0100

    Reverting change

--- CoreCodedChatbot.Web/Controllers/LoginController.cs
+++ CoreCodedChatbot.Web/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCodedChatbot.Helpers.Interfaces;
+using CoreCodedChatbot.Library.Models.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,13 @@ namespace CoreCodedChatbot.Web.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ConfigModel config;
+
+        public LoginController(IConfigHelper configHelper)
+        {
+            this.config = configHelper.GetConfig();
+        }
+
         public IActionResult Index()
         {
             return Challenge(new AuthenticationProperties() { RedirectUri = "/" });
