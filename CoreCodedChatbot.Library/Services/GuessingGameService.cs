a92b03356e2b849a780128429b07f026f9dd7497
Codedghost <codedghost2@gmail.com>
2018-10-01 20:12:22 +0100

    Fix for loss of fraction?

--- CoreCodedChatbot.Library/Services/GuessingGameService.cs
+++ CoreCodedChatbot.Library/Services/GuessingGameService.cs
@@ -180,7 +180,7 @@ namespace CoreCodedChatbot.Library.Services
             {
                 Usernames = winners.Select(w => w.Item2.Username).ToArray(),
                 Difference = firstWinner.Item1,
-                BytesWon = (decimal)(firstWinner.Item1 == 0 ? 1 / winners.Count :
+                BytesWon = (decimal)(firstWinner.Item1 == 0 ? 1.0 / winners.Count :
                     firstWinner.Item1 <= 1 ? 0.5 / winners.Count :
                     0.25 / winners.Count)
             };
