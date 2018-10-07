f20dbfc4d62e0006ac3abc98394797dca78987ac
Ran Rosenmann <ran.rosenmann@gmail.com>
2018-10-08 00:14:28 +0300

    added routing added songs-list component with mock data

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { MatInputModule, MatToolbarModule, MatButtonModule } from "@angular/material";
+import { MatInputModule, MatToolbarModule, MatButtonModule, MatTableModule, MatSortModule } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { SongsListComponent } from './songs-list/songs-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SongsListComponent],
   imports: [
     BrowserModule,
     MatInputModule,
     MatToolbarModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    MatTableModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
