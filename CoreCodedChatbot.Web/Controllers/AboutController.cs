81c655980e839cb447f0800c77dbfd57d77b9e44
codedghost2 <codedghost2@gmail.com>
2018-12-29 16:26:25 +0000

    Adding extra icons with attribution

---
+++ CoreCodedChatbot.Web/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodedChatbot.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public AboutController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
