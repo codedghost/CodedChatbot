00904f8fc75858b99d6806fcde81d96f530b5863
codedghost2 <codedghost2@gmail.com>
2018-12-30 19:28:09 +0000

    FormattedRequest is now more self contained and reusable

--- CoreCodedChatbot.Library/Models/Data/PlaylistItem.cs
+++ CoreCodedChatbot.Library/Models/Data/PlaylistItem.cs
@@ -6,12 +6,6 @@ namespace CoreCodedChatbot.Library.Models.Data
 {
     public class PlaylistItem
     {
-        private Regex songRequestRegex = new Regex(
-            "((?<index>[0-9]*)?(?<seperator> ?- ?)?(?<artistname>[\\d\\w ,.!\"£$%^&*=+\\\\\\/|<>?`¬[\\]{};\'#:@~]*)(?<seperator2> ?- ?)(?<songname>[\\d\\w ,.!\"£$%^&*\\(\\)=+\\\\\\/|<>?`¬[\\]{};\'#:@~]*)(?<instrument>(( ?- ?)?\\(?(bass|guitar)?\\)?)?))");
-
-        private Match regMatch => songRequestRegex.Match(songRequestText);
-        private string possibleInstrument => songRequestText.IndexOf("bass",  StringComparison.OrdinalIgnoreCase) >= 0 ? "bass" : "guitar";
-
         public int songRequestId { get; set; }
         public string songRequestText { get; set; }
         public string songRequester { get; set; }
@@ -19,14 +13,6 @@ namespace CoreCodedChatbot.Library.Models.Data
         public bool isVip { get; set; }
         public bool isEvenIndex { get; set; }
 
-        public FormattedRequest FormattedRequest =>
-            songRequestRegex.IsMatch(songRequestText)
-                ? new FormattedRequest
-                {
-                    SongName = regMatch.Groups["songname"].Captures.FirstOrDefault()?.Value,
-                    SongArtist = regMatch.Groups["artistname"].Captures.FirstOrDefault()?.Value,
-                    InstrumentName = possibleInstrument
-                }
-                : null;
+        public FormattedRequest FormattedRequest => FormattedRequest.GetFormattedRequest(songRequestText);
     }
 }
