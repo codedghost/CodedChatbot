1cb7bd5152c297baac89af30c796ce5d1ba1ee57
codedghost2 <codedghost2@gmail.com>
2018-12-31 13:45:57 +0000

    Edit command neatened up. No longer allows editing of the current song. Should hopefully be easier to trace

---
+++ CoreCodedChatbot.Library/Models/Enums/ProcessEditArgsResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCodedChatbot.Library.Models.Enums
+{
+    public enum ProcessEditArgsResult
+    {
+        RegularRequest,
+        VipRequestNoIndex,
+        VipRequestWithIndex,
+        OneRequestEdit,
+        ArgumentError,
+        NoRequestProvided,
+        NoRequestInList
+    }
+}
