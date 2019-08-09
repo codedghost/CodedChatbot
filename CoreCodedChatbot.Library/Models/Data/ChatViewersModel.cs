018db6379275131b3437ffdd04986d7e04507da5
codedghost2 <codedghost2@gmail.com>
2019-08-09 08:25:59 +0100

    Refactoring how we check if a user is a moderator or not

--- CoreCodedChatbot.Library/Models/Data/ChatViewersModel.cs
+++ CoreCodedChatbot.Library/Models/Data/ChatViewersModel.cs
@@ -1,10 +1,18 @@
-﻿namespace CoreCodedChatbot.Library.Models.Data
+﻿using System;
+using System.Linq;
+
+namespace CoreCodedChatbot.Library.Models.Data
 {
     public class ChatViewersModel
     {
         public object _links { get; set; }
         public int chatter_count { get; set; }
         public ChattersModel chatters { get; set; }
+
+        public bool IsUserMod(string username) => chatters?.moderators?.Any(mod =>
+                                                             string.Equals(mod, username,
+                                                                 StringComparison.CurrentCultureIgnoreCase)) ??
+                                                         false;
     }
 
     public class ChattersModel
