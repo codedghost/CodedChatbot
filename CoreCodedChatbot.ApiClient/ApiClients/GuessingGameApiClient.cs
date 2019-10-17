fc1251a23c66362ab1a183e0a6b6429f88cedf98
codedghost2 <codedghost2@gmail.com>
2019-10-17 21:13:06 +0100

    Implementing GuessingGameApiClient

--- CoreCodedChatbot.ApiClient/ApiClients/GuessingGameApiClient.cs
+++ CoreCodedChatbot.ApiClient/ApiClients/GuessingGameApiClient.cs
@@ -1,9 +1,54 @@
-﻿using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.Library.Helpers;
+using CoreCodedChatbot.Library.Interfaces.Services;
 using CoreCodedChatbot.Library.Models.ApiRequest.GuessingGame;
+using Newtonsoft.Json;
 
 namespace CoreCodedChatbot.ApiClient.ApiClients
 {
     public class GuessingGameApiClient : IGuessingGameApiClient
     {
+        private HttpClient _guessingGameClient;
+
+        public GuessingGameApiClient(IConfigService configService)
+        {
+            var config = configService.GetConfig();
+            _guessingGameClient = new HttpClient
+            {
+                BaseAddress = new Uri(config.GuessingGameApiUrl),
+                DefaultRequestHeaders =
+                {
+                    Authorization = new AuthenticationHeaderValue("Bearer", config.JwtTokenString)
+                }
+            };
+        }
+
+        public async Task<bool> StartGuessingGame(StartGuessingGameModel songInfo)
+        {
+            var result = await _guessingGameClient.PostAsync("StartGuessingGame",
+                HttpClientHelper.GetJsonData(songInfo));
+
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> FinishGuessingGame(decimal finalPercentage)
+        {
+            var result = await _guessingGameClient.PostAsync("FinishGuessingGame",
+                HttpClientHelper.GetJsonData(finalPercentage));
+
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SubmitGuess(SubmitGuessModel submitGuessModel)
+        {
+            var result = await _guessingGameClient.PostAsync("SubmitGuess",
+                HttpClientHelper.GetJsonData(submitGuessModel));
+
+            return result.IsSuccessStatusCode;
+        }
     }
 }
--- CoreCodedChatbot.ApiClient/Interfaces/ApiClients/IGuessingGameApiClient.cs
+++ CoreCodedChatbot.ApiClient/Interfaces/ApiClients/IGuessingGameApiClient.cs
@@ -1,11 +1,12 @@
-﻿using CoreCodedChatbot.Library.Models.ApiRequest.GuessingGame;
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.Library.Models.ApiRequest.GuessingGame;
 
 namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
 {
     public interface IGuessingGameApiClient
     {
-        bool StartGuessingGame(StartGuessingGameModel songInfo);
-        bool FinishGuessingGame(decimal finalPercentage);
-        bool SubmitGuess(SubmitGuessModel submitGuessModel);
+        Task<bool> StartGuessingGame(StartGuessingGameModel songInfo);
+        Task<bool> FinishGuessingGame(decimal finalPercentage);
+        Task<bool> SubmitGuess(SubmitGuessModel submitGuessModel);
     }
 }
