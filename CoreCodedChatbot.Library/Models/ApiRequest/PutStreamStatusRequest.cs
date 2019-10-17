d448ba2420ef08221eb63bfd15e8e5cb3188282f
codedghost2 <codedghost2@gmail.com>
2019-10-17 22:41:10 +0100

    Adding new Stream Status Request model

---
+++ CoreCodedChatbot.Library/Models/ApiRequest/PutStreamStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.Library.Models.ApiRequest
+{
+    public class PutStreamStatusRequest
+    {
+        public string BroadcasterUsername { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
