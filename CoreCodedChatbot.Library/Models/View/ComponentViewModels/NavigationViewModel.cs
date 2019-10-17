3d45f5a40fec5498421fe31a956c3d4dbc5e8652
codedghost2 <codedghost2@gmail.com>
2019-10-17 23:15:53 +0100

    Twitch info for playlist now only shows when broadcaster is live

---
+++ CoreCodedChatbot.Library/Models/View/ComponentViewModels/NavigationViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreCodedChatbot.Library.Models.View.ComponentViewModels
+{
+    public class NavigationViewModel
+    {
+        public bool IsBroadcasterOnline { get; set; }
+    }
+}
--- CoreCodedChatbot.Web/Components/NavViewComponent.cs
+++ CoreCodedChatbot.Web/Components/NavViewComponent.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.Library.Interfaces.Services;
+using CoreCodedChatbot.Library.Models.ApiRequest.StreamStatus;
+using CoreCodedChatbot.Library.Models.Data;
+using CoreCodedChatbot.Library.Models.View.ComponentViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodedChatbot.Web.Components
 {
     public class NavViewComponent : ViewComponent
     {
-        
+        private readonly IStreamStatusClient _streamStatusClient;
+        private ConfigModel _config;
+
+        public NavViewComponent(IStreamStatusClient streamStatusClient, IConfigService configService)
+        {
+            _streamStatusClient = streamStatusClient;
+
+            _config = configService.GetConfig();
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var streamStatus = await _streamStatusClient.GetStreamStatus(
+                new GetStreamStatusRequest
+                {
+                    BroadcasterUsername = _config.StreamerChannel
+                });
+            
+            return View(new NavigationViewModel
+            {
+                IsBroadcasterOnline = streamStatus.IsOnline
+            });
+        }
     }
 }
