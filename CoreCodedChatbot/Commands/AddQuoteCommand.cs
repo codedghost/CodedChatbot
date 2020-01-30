b8cef0a7f89dd3e0c63e6b0abc1b08e726b0e893
codedghost2 <codedghost2@gmail.com>
2020-01-30 00:40:49 +0000

    Can now add and get quotes from the API

---
+++ CoreCodedChatbot/Commands/AddQuoteCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.RequestModels.Quotes;
+using CoreCodedChatbot.Interfaces;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace CoreCodedChatbot.Commands
+{
+    [CustomAttributes.ChatCommand(new[] {"addquote"}, false)]
+    public class AddQuoteCommand : ICommand
+    {
+        private readonly IQuoteApiClient _quoteApiClient;
+
+        public AddQuoteCommand(IQuoteApiClient quoteApiClient)
+        {
+            _quoteApiClient = quoteApiClient;
+        }
+
+        public async void Process(TwitchClient client, string username, string commandText, bool isMod, JoinedChannel joinedChannel)
+        {
+            var request = new AddQuoteRequest
+            {
+                QuoteText = commandText,
+                Username = username
+            };
+
+            var quote = await _quoteApiClient.AddQuote(request);
+
+            if (quote == null)
+                client.SendMessage(joinedChannel,
+                    $"Hey @{username}, I had some trouble adding that to the database, please try again soon");
+
+            client.SendMessage(joinedChannel,
+                $"Hey @{username}, I have added Quote {quote.QuoteId}: {commandText}");
+        }
+
+        public void ShowHelp(TwitchClient client, string username, JoinedChannel joinedChannel)
+        {
+            client.SendMessage(joinedChannel, $"Hey @{username}, this command will add a quote to the database!");
+        }
+    }
+}
---
+++ CoreCodedChatbot/Commands/GetQuoteCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.RequestModels.Quotes;
+using CoreCodedChatbot.Interfaces;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace CoreCodedChatbot.Commands
+{
+    [CustomAttributes.ChatCommand(new[] { "quote" }, false)]
+    public class GetQuoteCommand : ICommand
+    {
+        private readonly IQuoteApiClient _quoteApiClient;
+
+        public GetQuoteCommand(IQuoteApiClient quoteApiClient)
+        {
+            _quoteApiClient = quoteApiClient;
+        }
+
+        public async void Process(TwitchClient client, string username, string commandText, bool isMod, JoinedChannel joinedChannel)
+        {
+            var parsed = int.TryParse(commandText, out var quoteId);
+            var request = new GetQuoteRequest
+            {
+                QuoteId = parsed ? quoteId : (int?)null
+            };
+
+            var quote = await _quoteApiClient.GetQuote(request);
+
+            if (quote == null)
+                client.SendMessage(joinedChannel,
+                    $"Hey @{username}, I had some trouble getting that Quote, please try again soon");
+
+            client.SendMessage(joinedChannel,
+                $"Hey @{username}, Here is Quote {quote.QuoteId}: {quote.QuoteText}");
+        }
+
+        public void ShowHelp(TwitchClient client, string username, JoinedChannel joinedChannel)
+        {
+            client.SendMessage(joinedChannel, $"Hey @{username}, this command will add a quote to the database!");
+        }
+    }
+}
