ac0eb8e4a7c14a490ccced495b60f6a674587474
codedghost2 <codedghost2@gmail.com>
2022-04-21 18:51:55 +0100

    Adding mod command to reset guessing game if it gets in a mess

---
+++ CoreCodedChatbot/Commands/FixGuessingGameCommand.cs
@@ -0,0 +1,39 @@
+﻿using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.RequestModels.Settings;
+using CoreCodedChatbot.Interfaces;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace CoreCodedChatbot.Commands
+{
+    [CustomAttributes.ChatCommand(new[] { "resetgg" }, true)]
+    public class FixGuessingGameCommand : ICommand
+    {
+        private readonly ISettingsApiClient _settingsApiClient;
+
+        public FixGuessingGameCommand(ISettingsApiClient settingsApiClient)
+        {
+            _settingsApiClient = settingsApiClient;
+        }
+
+        public async void Process(TwitchClient client, string username, string commandText, bool isMod, JoinedChannel joinedChannel)
+        {
+            var success = await _settingsApiClient.UpdateSettings(new UpdateSettingsRequest
+            {
+                Key = "IsGuessingGameInProgress",
+                Value = "false"
+            });
+
+            client.SendMessage(joinedChannel,
+                success
+                    ? $"Hey @{username}, I managed to reset the Guessing Game, try it now!"
+                    : $"Hey @{username}, I couldn't reset the Guessing Game, shit's fucked yo");
+        }
+
+        public void ShowHelp(TwitchClient client, string username, JoinedChannel joinedChannel)
+        {
+            client.SendMessage(joinedChannel,
+                $"Hey @{username}, this command lets mods fix the guessing game when it gets a bit wonky");
+        }
+    }
+}
